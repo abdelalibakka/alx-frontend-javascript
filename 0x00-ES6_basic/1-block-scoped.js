43c4bdd74af24537cd687c8d4de983aecebf5d03
Your Name <pakkatatata@gmail.com>
2024-01-03 22:33:51 +0000

    update

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -3,10 +3,10 @@ export default function taskBlock(trueOrFalse) {
   const task2 = true;
 
   if (trueOrFalse) {
-    // eslint disablenextline nounusedvars, no shadow
-    const task = true;
-    // eslintDisable-next line No unused vars
-    const task2 = false;
+    // eslint-disable-next-line
+		const task = true;
+    // eslint-disable-next-line
+		const task2 = false;
   }
 
   return [task, task2];
