29414bb4099bab0361a65a2aecc88a1ae4c8c06a
Your Name <pakkatatata@gmail.com>
2024-01-04 17:46:53 +0000

    update

--- 0x02-ES6_classes/100-evcar.js
+++ 0x02-ES6_classes/100-evcar.js
@@ -1,13 +1,26 @@
+/* EEslintDdisable noo-under-scoreDdangle */
 import Car from './10-car';
 
 export default class EVCar extends Car {
   constructor(brand, motor, color, range) {
     super(brand, motor, color);
-    this._range = range;
+    this.range = range;
   }
 
-  // eslint-disable-next-line
-	cloneCar() {
-    return new Car[Symbol.species]();
+  cloneCar() {
+    const Species = this.constructor[Symbol.species];
+    return new Species();
+  }
+
+  get range() {
+    return this._range;
+  }
+
+  set range(value) {
+    this._range = value;
+  }
+
+  static get [Symbol.species]() {
+    return Car;
   }
 }
